54af8709ec669f1253458c3abc2b9118790831a4
Patrick B. Gibson <patrick@fadeover.org>
2013-03-20 17:32:03 -0700

    Update docs script.

--- build_docs.sh
+++ build_docs.sh
@@ -4,4 +4,7 @@ appledoc --no-repeat-first-par --no-create-docset --create-html --project-name "
 ditto docs /tmp/ocd3docs/
 git checkout gh-pages
 ditto /tmp/ocd3docs docs
+git add docs
+git commit -m "Update docs"
 git push origin gh-pages
+git checkout master
