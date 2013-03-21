89dac03ec27d8753b021ec4c4059dd6e2631217d
Patrick B. Gibson <patrick@fadeover.org>
2013-03-21 15:42:09 -0700

    Update to build docs.

--- build_docs.sh
+++ build_docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 appledoc --no-repeat-first-par --no-create-docset --create-html --project-name "OCD3" --project-company "Tilde Inc" --company-id io.tilde --output docs .
-ditto docs /tmp/ocd3docs/
+ditto docs/html /tmp/ocd3docs/
 git checkout gh-pages
 ditto /tmp/ocd3docs docs
 git add docs
